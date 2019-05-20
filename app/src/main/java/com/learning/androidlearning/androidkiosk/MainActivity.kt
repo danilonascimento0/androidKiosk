41651ad8bad436a425062dc72e50f5458527c059
danilonascimento0 <dbnascimentto@gmail.com>
2019-05-20 11:17:41 -0300

    Created call for Browser and Calculator

--- app/src/main/java/com/learning/androidlearning/androidkiosk/MainActivity.kt
+++ app/src/main/java/com/learning/androidlearning/androidkiosk/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.learning.androidlearning.androidkiosk
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Build
@@ -12,22 +14,22 @@ class MainActivity : AppCompatActivity() {
     var kiosk: Kiosk = Kiosk(this)
     //var isKioskActive: Boolean = false
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-            txAndroidVersion.text = Build.VERSION.SDK_INT.toString()
-            txAndroidRelease.text = Build.VERSION.RELEASE.toString()
+        txAndroidVersion.text = Build.VERSION.SDK_INT.toString()
+        txAndroidRelease.text = Build.VERSION.RELEASE.toString()
 
-            kiosk.hideSystemUI(window)
-            setKioskMode()
-        }
+        kiosk.hideSystemUI(window)
+        setKioskMode()
+    }
 
-        private fun setKioskMode() {
-            kiosk.startKioskMode()
-            btKiosk.text = "Desativar Kiosk"
-            txAtivo.text = "ATIVO"
-            //isKioskActive = true
+    private fun setKioskMode() {
+        kiosk.startKioskMode()
+        btKiosk.text = "Desativar Kiosk"
+        txAtivo.text = "ATIVO"
+        //isKioskActive = true
 
         btKiosk.setOnClickListener {
             removeKioskMode()
@@ -54,6 +56,20 @@ class MainActivity : AppCompatActivity() {
                 //kiosk.showSystemUI(window)
             //}
         }
+    }
+
+    fun openCalculator(@Suppress("UNUSED_PARAMETER") view: View) {
+        val intent = Intent()
+        intent.action = Intent.ACTION_MAIN
+        intent.addCategory(Intent.CATEGORY_APP_CALCULATOR)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
+    fun openBrowser(@Suppress("UNUSED_PARAMETER") view: View) {
+        val openURL = Intent(Intent.ACTION_VIEW)
+        openURL.data = Uri.parse("https://www.tutorialkart.com/")
+        startActivity(openURL)
     }
 
     fun closeApplication(@Suppress("UNUSED_PARAMETER") view: View) {
